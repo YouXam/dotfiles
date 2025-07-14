c023f95505decc30210408006a76097c802bcab5
YouXam <youxam@outlook.com>
2025-07-14 13:37:05 +0800

    update abbr.fish

--- dot_config/private_fish/custom/abbr.fish
+++ dot_config/private_fish/custom/abbr.fish
@@ -4,9 +4,11 @@ abbr --add cz chezmoi
 abbr --add g git
 abbr --add ga git add
 abbr --add gc git commit
-abbr --add gp git pull
+abbr --add gpl git pull
+abbr --add gps git push
 abbr --add gs git status
 abbr --add cga chezmoi git add
 abbr --add cgc chezmoi git commit
-abbr --add cgp chezmoi git pull
+abbr --add cgpl chezmoi git pull
+abbr --add cgps chezmoi git push
 abbr --add cgs chezmoi git status
