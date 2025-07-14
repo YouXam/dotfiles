3523385bdc3b1557e130c728c905dff0fc7a97b4
YouXam <youxam@outlook.com>
2025-07-14 13:36:02 +0800

    update abbr

--- dot_config/private_fish/custom/abbr.fish
+++ dot_config/private_fish/custom/abbr.fish
@@ -1,3 +1,12 @@
 abbr --add zj zellij
 abbr --add k kubectl
 abbr --add cz chezmoi
+abbr --add g git
+abbr --add ga git add
+abbr --add gc git commit
+abbr --add gp git pull
+abbr --add gs git status
+abbr --add cga chezmoi git add
+abbr --add cgc chezmoi git commit
+abbr --add cgp chezmoi git pull
+abbr --add cgs chezmoi git status
